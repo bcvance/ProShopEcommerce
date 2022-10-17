8f16d373130b3cea2aaa46a68b88843539009398
bcvance <81263222+bcvance@users.noreply.github.com>
2022-10-16 20:49:44 -0500

    added update profile

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -6,6 +6,7 @@ from base.views import user_views as views
 urlpatterns = [
     path('', views.getUsers, name='users'),
     path('profile/', views.getUserProfile, name='users_profile'),
+    path('profile/update/', views.updateUserProfile, name='user_profile_update'),
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('register/', views.registerUser, name='register'),
 ]
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,7 +3,18 @@ import {
     USER_LOGIN_SUCCESS,
     USER_LOGIN_FAIL,
     USER_LOGIN_FAIL_400,
-    USER_LOGOUT 
+
+    USER_LOGOUT,
+    
+    USER_REGISTER_REQUEST,
+    USER_REGISTER_SUCCESS,
+    USER_REGISTER_FAIL,
+    USER_REGISTER_FAIL_400,
+
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
+    USER_DETAILS_FAIL,
+    USER_DETAILS_FAIL_400,
 } from '../constants/userConstants'
 
 export const login = (email, password) => async (dispatch) => {
@@ -38,6 +49,100 @@ export const login = (email, password) => async (dispatch) => {
     }catch(error) {
         dispatch({
             type: USER_LOGIN_FAIL,
+            payload: error.response && error.response.data.detail
+            ? error.response.data.detail
+            : error.message
+        })
+    }
+}
+
+
+export const logout = () => (dispatch) => {
+    localStorage.removeItem('userInfo')
+    dispatch({type: USER_LOGOUT})
+}
+
+export const register = (name, email, password) => async (dispatch) => {
+    try {
+        dispatch({
+            type: USER_REGISTER_REQUEST
+        })
+
+        let url = 'http://127.0.0.1:8000/api/users/register/'
+        let response = await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({'name': name, 'email': email, 'password': password}),
+            headers: {'Content-Type': 'application/json'}
+        })
+        let data = await response.json()
+        if (response.ok) {
+
+
+            dispatch({
+                type:USER_REGISTER_SUCCESS,
+                payload:data
+            })
+
+            dispatch({
+                type: USER_LOGIN_SUCCESS,
+                payload: data
+            })
+            localStorage.setItem('userInfo', JSON.stringify(data))
+        }
+        else {
+            dispatch({
+                type: USER_REGISTER_FAIL_400,
+                payload:data.detail
+            })
+        }
+
+    }catch(error) {
+        dispatch({
+            type: USER_REGISTER_FAIL,
+            payload: error.response && error.response.data.detail
+            ? error.response.data.detail
+            : error.message
+        })
+    }
+}
+
+export const getUserDetails = (id) => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: USER_DETAILS_REQUEST
+        })
+
+        const { 
+            userLogin: { userInfo}
+         } = getState()
+
+        let url = `http://127.0.0.1:8000/api/users/${id}/`
+        let response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        })
+        let data = await response.json()
+        if (response.ok) {
+
+
+            dispatch({
+                type:USER_DETAILS_SUCCESS,
+                payload:data
+            })
+        }
+        else {
+            dispatch({
+                type: USER_DETAILS_FAIL_400,
+                payload:data.detail
+            })
+        }
+
+    }catch(error) {
+        dispatch({
+            type: USER_DETAILS_FAIL,
             payload: error.response && error.response.data.detail
             ? error.response.data.detail
             : error.message
